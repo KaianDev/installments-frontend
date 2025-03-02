0863c8acc3386a04e50bd89a375dad9d9d6be78f
KaianDev <kaianvasconcelos@gmail.com>
2025-03-02 19:49:22 -0300

    style: adjust layout and responsiveness to home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -6,7 +6,7 @@ import { FRONTEND_ROUTES } from "@/constants"
 
 const Home = () => {
   return (
-    <div className="grid h-full min-h-screen grid-cols-2 gap-8 p-8">
+    <div className="grid h-full min-h-screen gap-8 p-8 lg:grid-cols-2">
       <main className="flex flex-col items-center justify-evenly gap-12">
         <header className="flex">
           <Link href={FRONTEND_ROUTES.HOME.href}>
@@ -18,11 +18,13 @@ const Home = () => {
             />
           </Link>
         </header>
-        <div className="flex max-w-md flex-col gap-4">
+        <div className="flex flex-col gap-4 lg:max-w-md">
           <div className="flex items-baseline gap-2">
-            <p className="text-[2.5rem] font-semibold">Bem vindo de volta 👋</p>
+            <p className="text-3xl font-semibold lg:text-[2.5rem]">
+              Bem vindo de volta 👋
+            </p>
           </div>
-          <div className="text-muted-foreground text-lg leading-5">
+          <div className="text-muted-foreground text-sm leading-5 sm:text-base lg:text-lg">
             <p>
               Seu futuro financeiro começa aqui. Transforme números em decisões.
             </p>
@@ -34,7 +36,7 @@ const Home = () => {
             <div className="bg-muted h-px flex-1" />
           </div>
           <LoginWithGoogle />
-          <p className="text-center">
+          <p className="text-center text-sm sm:text-base">
             Ainda não possui uma conta?{" "}
             <Link href={FRONTEND_ROUTES.REGISTER.href} className="link">
               Registre-se já
@@ -42,11 +44,11 @@ const Home = () => {
             .
           </p>
         </div>
-        <footer>
+        <footer className="text-muted-foreground text-sm">
           &copy; {new Date().getFullYear()} - Todos os direitos reservados
         </footer>
       </main>
-      <section className="relative flex h-full items-center justify-center overflow-hidden rounded-3xl p-2">
+      <section className="relative hidden h-full items-center justify-center overflow-hidden rounded-3xl p-2 lg:flex">
         <Image
           src="/assets/images/bg-login.jpg"
           alt=""
